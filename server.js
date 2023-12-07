d93c4f710397dc19557bb73319dbafbe045bab57
Mariane Limoli <marianelimoli@gmail.com>
2023-12-07 14:10:29 -0300

    commit

--- server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./src/app");
-var PORTA = 3000;
-app_1.default.listen(PORTA, function () {
-    console.log("Servidor executando em http://localhost:".concat(PORTA));
-});

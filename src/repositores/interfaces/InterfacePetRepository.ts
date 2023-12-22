ee21c43880916f352a932713b8ca8ca096879f94
Mariane Limoli <marianelimoli@gmail.com>
2023-12-22 09:52:25 -0300

    crud

--- src/repositores/interfaces/InterfacePetRepository.ts
+++ src/repositores/interfaces/InterfacePetRepository.ts
@@ -3,6 +3,10 @@ import PetEntity from "../../entities/PetEntity";
 export default interface InterfacePetRepository {
   criaPet(pet: PetEntity): void;
   listaPets(): PetEntity[] | Promise<PetEntity[]>;
-  atualizaPet(id: number, pet: PetEntity): void;
-  deletaPet(id: number, pet: PetEntity): void;
+  atualizaPet(
+    id: number,
+    pet: PetEntity
+  ): Promise<{ success: boolean; message?: string }> | void;
+
+  deletaPet(id: number): Promise<{ success: boolean; message?: string }> | void;
 }
